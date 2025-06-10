d924ecb2d2fd956bcb83b619f9cf41658941fa11
c0a242603f <c0a242603f@edu.teu.ac.jp>
2025-06-10 15:27:16 +0900

    練習問題3

--- pygame_image.py
+++ pygame_image.py
@@ -10,6 +10,8 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    koukaton3_img = pg.image.load("3.png")
+    koukaton3_img = pg.transform.flip(koukaton3_img, True, False)
     tmr = 0
     while True:
         for event in pg.event.get():
