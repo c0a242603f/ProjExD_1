b557f01bcf7e1d370af3b2ee2bb5a8e3ccd0fa1e
c0a242603f <c0a242603f@edu.teu.ac.jp>
2025-06-10 17:08:52 +0900

    演習課題1

--- flying_kokaton.py
+++ flying_kokaton.py
@@ -19,17 +19,35 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+
+
+        
         key_lst = pg.key.get_pressed()
 
-        if key_lst[pg.K_UP]:  # 上キーが押されたらAdd commentMore actions
-            koukaton3_rct.move_ip(0, -1)  # 上に移動
-        if key_lst[pg.K_DOWN]:
-            koukaton3_rct.move_ip(0, +1)
-        if key_lst[pg.K_LEFT]:
-            koukaton3_rct.move_ip(-1, 0)
-        if key_lst[pg.K_RIGHT]:
-            koukaton3_rct.move_ip(+1, 0)
+        x = -2
+        y = 0
 
+        if key_lst[pg.K_UP]:
+            x = 0
+            y = -2  
+              
+        if key_lst[pg.K_DOWN]:
+            x = 0
+            y = +2
+            
+        if key_lst[pg.K_LEFT]:
+            x = -2
+            y = 0
+            
+        if key_lst[pg.K_RIGHT]:
+            x = +5
+            y = 0
+            
+        
+        koukaton3_rct.move_ip(x, y)
+        
+
+        
 
         x = tmr
         x = tmr%3200
@@ -37,16 +55,18 @@ def main():
         screen.blit(bg_img, [-x, 0])  # 1枚目
         screen.blit(bg_img2, [-x+1600, 0])  # 2枚目
         screen.blit(bg_img, [-x+3200, 0])  # 3枚目
-       
+
+        
+
         screen.blit(koukaton3_img, koukaton3_rct)
-        koukaton3_rct = koukaton3_img.get_rect()
-        koukaton3_rct.center = 300, 200
+        #koukaton3_rct = koukaton3_img.get_rect()
+        #koukaton3_rct.center = 300, 200
         
 
         pg.display.update()
         
         tmr += 1        
-        clock.tick(100)
+        clock.tick(200)
 
 
 if __name__ == "__main__":
