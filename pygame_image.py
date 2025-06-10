e50c63feeaf5c42d6519f8e39cd019bcf99a91c5
c0a242603f <c0a242603f@edu.teu.ac.jp>
2025-06-10 15:52:46 +0900

    練習問題8

--- pygame_image.py
+++ pygame_image.py
@@ -10,6 +10,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img2 = pg.transform.flip(bg_img, True, False)
     koukaton3_img = pg.image.load("fig/3.png")
     koukaton3_img = pg.transform.flip(koukaton3_img, True, False)
     tmr = 0
@@ -20,7 +21,7 @@ def main():
 
         
         screen.blit(bg_img, [-x, 0])  # 1枚目
-        screen.blit(bg_img, [-x+1600, 0])  # 2枚目
+        screen.blit(bg_img2, [-x+1600, 0])  # 2枚目
         screen.blit(koukaton3_img, [300, 200])
         koukaton3_img
         pg.display.update()
