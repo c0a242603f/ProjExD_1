bf4cfc910855b047bd57f873e2ea8f7132dd9090
c0a242603f <c0a242603f@edu.teu.ac.jp>
2025-06-10 16:06:34 +0900

    練習問題9

--- pygame_image.py
+++ pygame_image.py
@@ -18,13 +18,16 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         x = tmr
-
+        x = tmr%3200
         
         screen.blit(bg_img, [-x, 0])  # 1枚目
         screen.blit(bg_img2, [-x+1600, 0])  # 2枚目
+        screen.blit(bg_img, [-x+3200, 0])  # 3枚目
         screen.blit(koukaton3_img, [300, 200])
-        koukaton3_img
+        
+
         pg.display.update()
+        
         tmr += 1        
         clock.tick(100)
 
