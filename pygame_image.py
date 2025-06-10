db7f8ab9570189461fb1a10948c9acefa304008f
c0a242603f <c0a242603f@edu.teu.ac.jp>
2025-06-10 15:40:48 +0900

    練習問題5

--- pygame_image.py
+++ pygame_image.py
@@ -16,8 +16,9 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        screen.blit(bg_img, [0, 0])
+        screen.blit(bg_img, [-tmr, 0])
         screen.blit(koukaton3_img, [300, 200])
+        koukaton3_img
         pg.display.update()
         tmr += 1        
         clock.tick(10)
